225c0533d03617bdad019200dd11fbd9fcdaa1cf
wanziyang12 <58157404+wanziyang12@users.noreply.github.com>
2019-11-25 11:58:08 +0800

    Update config.js

--- config.js
+++ config.js
@@ -24,6 +24,6 @@ var config = {
         cake_fadein: "蛋糕？",
         light_candle: "蜡烛？",
         wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        story: "I LOVE YOU FOREVER",
     }
 };
